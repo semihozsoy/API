20a7fd29782f0c3070ee0c95c1cc9c6b5e7293a4
Semih Özsoy <semih__ozsoy@hotmail.com>
2023-09-15 15:59:33 +0300

    request method implemented

---
+++ Sources/API/RequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  RequestMethod.swift
+//  
+//
+//  Created by Semih Ozsoy on 9.08.2023.
+//
+
+import Foundation
+
+public enum RequestMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch =  "PATCH"
+}
