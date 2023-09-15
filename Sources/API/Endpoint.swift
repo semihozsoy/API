9145794fdb19021a18f35ebff5c93b2bbbebea19
Semih Özsoy <semih__ozsoy@hotmail.com>
2023-09-15 15:59:01 +0300

    endpoint implemented for dynamic endpoints

---
+++ Sources/API/Endpoint.swift
@@ -0,0 +1,28 @@
+//
+//  Endpoint.swift
+//  
+//
+//  Created by Semih Ozsoy on 9.08.2023.
+//
+
+import Foundation
+
+public protocol Endpoint {
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var method: RequestMethod { get }
+    var header: [String: String]? { get }
+    var body: [String: String]? { get }
+    var queryItems: [URLQueryItem]? { get }
+}
+
+extension Endpoint {
+    public var scheme: String {
+        return "https"
+    }
+    
+    public var host: String {
+        return "api.themoviedb.org"
+    }
+}
